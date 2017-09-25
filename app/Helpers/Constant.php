798c55e9591f4ef9c8bc27cbacef2fd6310026bf
DaiDV <daidv@rikkeisoft.com>
2017-09-25 23:47:15 +0700

    develop: Add Helpers Library: common, constant, message

---
+++ app/Helpers/Constant.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Helpers of constant
+ *
+ * @author DaiDV
+ */
+define('ADMIN', 1);
+define('USER', 0);
---
+++ app/Helpers/Msg.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Helpers of message
+ *
+ * @author DaiDV
+ */
+class Msg {
+
+  //common
+  const ALERT_SUCCESS = 'Success, Complete!';
+}
